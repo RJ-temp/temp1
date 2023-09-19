e9c41b93c5a2b91e7d86fd3fb65e7f7e6f932eb2
RJ <ritikjuneja9897@gmail.com>
2023-09-19 08:32:34 +0530

    Part 1 completed

--- server/src/messages/messages.service.ts
+++ server/src/messages/messages.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { MessageRepository } from './messages.repository';
 
 @Injectable()
-export class MessagesService {}
+export class MessagesService {
+    constructor(public messagerepo : MessageRepository){}
+
+    findOne(id :string){
+        return this.messagerepo.findOne(id);
+    }
+    findAll(){
+        return this.messagerepo.findAll();
+    }
+    create(message:string){
+        return this.messagerepo.create(message);
+    }
+}
