20d014c35702dd926cbb15ac193aa2408901d2c0
RJ <ritikjuneja9897@gmail.com>
2023-09-20 09:31:34 +0530

    Just before applying Interceptor

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post , Patch , Delete , Param , Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -8,5 +9,21 @@ export class UsersController {
     @Post('/signup')
     CreateUser(@Body() body:CreateUserDto){
         this.userService.create(body.email , body.password);
+    }
+    @Get('/:id')
+    findUser(@Param('id') id:string){
+        return this.userService.findOne(parseInt(id));
+    }
+    @Get()
+    findAllUser(@Query('email') email:string){
+        return this.userService.find(email);
+    }
+    @Delete('/:id')
+    deleteUser(@Param('id') id:string){
+        this.userService.remove(parseInt(id));
+    }
+    @Patch('/:id')
+    updateUser(@Param('id') id:string , @Body() body:UpdateUserDto){
+        return this.userService.update(parseInt(id) , body);
     }
 }
