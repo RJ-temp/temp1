cec19d2c0335353696807045765a4ce2c04c89dc
RJ <ritikjuneja9897@gmail.com>
2023-09-20 14:56:22 +0530

    Interceptor Completed

--- server/src/interceptors/serialize.intercept.ts
+++ server/src/interceptors/serialize.intercept.ts
@@ -2,13 +2,23 @@ import { UseInterceptors,NestInterceptor,CallHandler, ExecutionContext } from "@
 import { Observable } from 'rxjs';
 import { map } from "rxjs/operators"; 
 import { plainToClass } from "class-transformer";
-import { UserDto } from "src/users/dtos/user.dto";
+
+interface ClassConstructor{
+    new(...args: any[]):{}
+}
+
+export function Serialize(dto:ClassConstructor){
+    return UseInterceptors(new SerializeInterceptor(dto))
+}
 
 export class SerializeInterceptor implements NestInterceptor {
+    constructor(private dto:any){
+
+    }
     intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
 
         return next.handle().pipe(map((data:any)=>{
-            return plainToClass(UserDto,data,{
+            return plainToClass(this.dto,data,{
                 excludeExtraneousValues:true
             })
         }))
--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -2,16 +2,19 @@ import { Body, Controller, Get, Post , Patch , Delete , Param , Query ,UseInterc
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
-import { SerializeInterceptor } from 'src/interceptors/serialize.intercept';
+import { Serialize } from 'src/interceptors/serialize.intercept';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)   //Apply to whole controller
 export class UsersController {
     constructor(private userService : UsersService){}
     @Post('/signup')
     CreateUser(@Body() body:CreateUserDto){
         this.userService.create(body.email , body.password);
     }
-    @UseInterceptors(SerializeInterceptor)
+    // @UseInterceptors(new SerializeInterceptor(UserDto))
+    // @Serialize(UserDto)
     @Get('/:id')
     findUser(@Param('id') id:string){
         console.log('Hnadler is running');
